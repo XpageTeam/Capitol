a9c6f7be0f27aa8ddd7bb946b9e43030942e49ba
danil.khandys <danil.khandys@xpage.ru>
2019-12-13 15:52:27 +0500

    Сверстал акции и подверстал всякое

--- src/ts/main-page.ts
+++ src/ts/main-page.ts
@@ -5,14 +5,14 @@ import App from "./xpage/core";
 Swiper.use([Lazy, Navigation, EffectFade, Keyboard, Autoplay]);
 
 domReady(() => {
-    App.each(".main-slide__content-text", (slideText: HTMLElement) => {
-        const curTextArray = slideText.innerText.split("");
+    // App.each(".main-slide__content-text", (slideText: HTMLElement) => {
+    //     const curTextArray = slideText.innerText.split("");
 
-        slideText.innerText = "";
+    //     slideText.innerText = "";
 
-        for (let i = 0; i < curTextArray.length; i++)
-            slideText.innerHTML+= `<span style="transition-delay: .${i+1}s">${curTextArray[i]}</span>`;
-    });
+    //     for (let i = 0; i < curTextArray.length; i++)
+    //         slideText.innerHTML+= `<span style="transition-delay: .${i+1}s">${curTextArray[i]}</span>`;
+    // });
 
     new Swiper(".main-slider", {
         effect: "fade",
